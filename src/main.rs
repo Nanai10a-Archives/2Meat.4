faf63f8e626db207ba497f0bc88455f253b4dd80
Nanai10a <Nanai10a@tutanota.com>
2020-11-02 16:19:25 +0900

    [fix]ignored and more

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,50 @@
-extern crate serenity;
+extern crate two_meat_rust;
 
-use serenity::prelude::*;
+use log::{error, info, LevelFilter};
+use serenity::client::bridge::gateway::GatewayIntents;
+use serenity::client::ClientBuilder;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::prelude::{Context, EventHandler};
+use serenity::{async_trait, Error};
+use simplelog::Config;
+use threadpool::ThreadPool;
 
-fn main() {
-    // Configure the client with your Discord bot token in the environment.
-    let token = "NzYzMjMyMTE5ODQwNTcxNDIy.X30tTQ.gXdMOWJpuBxRIJqIsbMg6BO7FvM";
+#[tokio::main]
+async fn main() {
+    simplelog::SimpleLogger::init(LevelFilter::Info, Config::default()).expect("err!");
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut client = Client::new(&token, Handler).expect("Err creating client");
+    info!("initialized env_logger");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
-    if let Err(why) = client.start() {
-        println!("Client error: {:?}", why);
+    let mut client =
+        ClientBuilder::new("")
+            .guild_subscriptions(true)
+            .event_handler(EventPoster)
+            .await;
+    info!("created client");
+
+    let mut _client = match client {
+        Ok(ok) => ok,
+        Err(err) => {
+            return error!("{}", err.to_string().as_str());
+        }
+    };
+
+    match _client.start().await {
+        Ok(_) => {}
+        Err(err) => {
+            return error!("{}", err.to_string().as_str());
+        }
     }
 }
 
-struct Handler;
+struct EventPoster;
 
-impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
-    fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-
-            if let Err(why) = msg.channel_id.say(ctx.http, "Pong!") {
-                println!("Error sending message: {:?}", why);
-            }
+#[async_trait]
+impl EventHandler for EventPoster {
+    async fn message(&self, ctx: Context, message: Message) {
+        if message.content == "!ping" {
+            message.channel_id.say(ctx.http, "pong!").await;
         }
-    }
-
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
-    fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
     }
 }
