ba009c634b306dc66d9d3b65dc8ee22f35352c75
Nanai10a <Nanai10a@tutanota.com>
2020-11-08 02:36:18 +0900

    :tada: projectの初期化をした.

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate two_meat_rust;
+
+use serenity::client::bridge::gateway::GatewayIntents;
+use serenity::client::ClientBuilder;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::{Context, EventHandler};
+use serenity::{async_trait, Error};
+use threadpool::ThreadPool;
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let mut client =
+        ClientBuilder::new(std::env::var("DISCORD_TOKEN").expect("Error: token not found!"))
+            .guild_subscriptions(true)
+            .event_handler(EventPoster)
+            .await;
+
+    let mut _client = match client {
+        Ok(ok) => ok,
+        Err(err) => {
+            panic!(err.to_string());
+        }
+    };
+
+    match _client.start().await {
+        Ok(_) => {}
+        Err(err) => {
+            panic!(err.to_string());
+        }
+    }
+}
+
+struct EventPoster;
+
+#[async_trait]
+impl EventHandler for EventPoster {
+    async fn message(&self, ctx: Context, message: Message) {
+        if message.content == "!ping" {
+            message.channel_id.say(ctx.http, "pong!").await;
+        }
+    }
+}
