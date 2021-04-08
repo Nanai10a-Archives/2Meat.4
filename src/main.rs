6631c3f7ed5cfad310c2bb594d5f13b670c65ed0
Nanai10a <Nanai10a@outlook.jp>
2021-04-09 04:32:14 +0900

    🚧 discord::initの呼び出し.

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ use two_meat_rust::prelude::*;
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
+
+    discord::init(std::env::var("DISCORD_TOKEN").expect("Error: token not found!"))
+        .await
+        .unwrap();
 }
