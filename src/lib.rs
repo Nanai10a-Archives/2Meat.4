acd7d049844a64785b8337d83277f47c4e87c361
Nanai10a <Nanai10a@outlook.jp>
2021-04-16 01:24:16 +0900

    💩 やはり要らなかった.

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,3 @@ pub mod interface;
 pub mod model;
 pub mod prelude;
 pub mod utils;
-
-fn unchecked<E>() -> Box<dyn Fn(E)> {
-    Box::new(|_| unimplemented!("implementer was not checked this handling... sorry."))
-}
-// .unwrap_or_else(crate::unchecked().as_ref())
