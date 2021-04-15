ce27fa1a8445439f3bbdb2f4e333dace925c9117
Nanai10a <Nanai10a@outlook.jp>
2021-04-15 16:33:57 +0900

    🩹 型の修正.

--- src/discord/impl_commands.rs
+++ src/discord/impl_commands.rs
@@ -2,39 +2,40 @@ use crate::commands;
 use crate::discord::receivers::{DiscordReceiver, DiscordReceivers};
 use crate::discord::senders::{DiscordSender, DiscordSenders};
 use crate::model::arg::Place;
+use crate::utils::RefWrap;
 
 impl commands::New<DiscordSenders> for DiscordSender {
-    fn new(_parent: impl AsRef<DiscordSenders>, _place: Place) -> anyhow::Result<Box<Self>> {
+    fn new(_parent: impl AsRef<DiscordSenders>, _place: Place) -> anyhow::Result<RefWrap<Self>> {
         todo!()
     }
 }
 
 impl commands::Drop for DiscordSender {
-    fn drop(self) -> anyhow::Result<()> {
+    fn drop(_self_: RefWrap<Self>) -> anyhow::Result<()> {
         todo!()
     }
 }
 
 impl commands::New<DiscordReceivers> for DiscordReceiver {
-    fn new(_parent: impl AsRef<DiscordReceivers>, _place: Place) -> anyhow::Result<Box<Self>> {
+    fn new(_parent: impl AsRef<DiscordReceivers>, _place: Place) -> anyhow::Result<RefWrap<Self>> {
         todo!()
     }
 }
 
 impl commands::Drop for DiscordReceiver {
-    fn drop(self) -> anyhow::Result<()> {
+    fn drop(_self_: RefWrap<Self>) -> anyhow::Result<()> {
         todo!()
     }
 }
 
 impl commands::Subsc<DiscordSender> for DiscordReceiver {
-    fn subsc(&self, _target: impl AsRef<DiscordSender>) -> anyhow::Result<()> {
+    fn subsc(&mut self, _target: impl AsRef<DiscordSender>) -> anyhow::Result<()> {
         todo!()
     }
 }
 
 impl commands::Exit<DiscordSender> for DiscordReceiver {
-    fn exit(&self, _target: impl AsRef<DiscordSender>) -> anyhow::Result<()> {
+    fn exit(&mut self, _target: impl AsRef<DiscordSender>) -> anyhow::Result<()> {
         todo!()
     }
 }
