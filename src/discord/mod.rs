de997c5f45da0b32344744b83be34091aeb04aed
Nanai10a <Nanai10a@outlook.jp>
2021-04-15 21:40:40 +0900

    🚧 serenityの開始処理を変更.

--- src/discord/mod.rs
+++ src/discord/mod.rs
@@ -1,5 +1,3 @@
-use crate::model::Error;
-use serenity::async_trait;
 use serenity::client::ClientBuilder;
 use serenity::model::prelude::Message;
 use serenity::prelude::{Context, EventHandler};
@@ -11,24 +9,10 @@ pub mod transferer;
 
 pub async fn init(token: impl AsRef<str>) -> anyhow::Result<()> {
     let mut client = ClientBuilder::new(token)
-        .guild_subscriptions(true)
-        .event_handler(EventPoster)
+        // .event_handler(DiscordInterface {})
         .await
         .unwrap();
 
-    match client.start().await {
-        Ok(_) => Ok(()),
-        Err(err) => Err(Error::Serenity(err)),
-    }
-}
-
-struct EventPoster;
-
-#[async_trait]
-impl EventHandler for EventPoster {
-    async fn message(&self, ctx: Context, message: Message) {
-        if message.content == "!ping" {
-            message.channel_id.say(ctx.http, "pong!").await.unwrap();
-        }
-    }
+    tokio::spawn(async move { client.start().await.unwrap() });
+    Ok(())
 }
