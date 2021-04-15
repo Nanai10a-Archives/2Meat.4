7b04711ba4ec4d42c431871dc556cde130883a59
Nanai10a <Nanai10a@outlook.jp>
2021-04-15 20:07:54 +0900

    🎨 冗長なGenericsの削除.

--- src/interface.rs
+++ src/interface.rs
@@ -1,4 +1,6 @@
-pub trait Interface<RD, SD> {
-    fn receive(&self, data: RD) -> anyhow::Result<()>;
-    fn send(&self, data: SD) -> anyhow::Result<()>;
+use crate::model::data::FormattedData;
+
+pub trait Interface {
+    fn receive(&self, data: FormattedData) -> anyhow::Result<()>;
+    fn send(&self, data: FormattedData) -> anyhow::Result<()>;
 }
