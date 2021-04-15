1e41af5b981001a1c99067693b25f5102b0a1f3f
Nanai10a <Nanai10a@outlook.jp>
2021-04-15 21:43:06 +0900

    🗑 ..or_elseに使えるFnを返す馬鹿っぽい関数を作成.

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,8 @@ pub mod interface;
 pub mod model;
 pub mod prelude;
 pub mod utils;
+
+fn unchecked<E>() -> Box<dyn Fn(E) -> ()> {
+    Box::new(|_| unimplemented!("implementer was not checked this handling... sorry."))
+}
+// .unwrap_or_else(crate::unchecked().as_ref())
