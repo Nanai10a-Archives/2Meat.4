beb8e22246331b7532586a7fda51643505728329
Nanai10a <Nanai10a@outlook.jp>
2021-04-20 08:30:48 +0900

    🚧 new_commandの実装.

--- src/discord/impl_commands.rs
+++ src/discord/impl_commands.rs
@@ -1,1 +1,26 @@
+use crate::commands::*;
+use crate::discord::transceiver::{
+    DiscordTransceiver, DiscordTransceivers, Transceiver, Transceivers,
+};
+use crate::utils::RefWrap;
+use anyhow::Error;
+use serenity::prelude::Mutex;
+use std::sync::Arc;
 
+#[serenity::async_trait]
+impl NewCommand<RefWrap<DiscordTransceivers>> for DiscordTransceiver {
+    async fn new_com(parent: &RefWrap<DiscordTransceivers>) -> anyhow::Result<RefWrap<Self>> {
+        match {
+            match (*parent.lock().await).as_mut() {
+                None => todo!(),
+                Some(item) => item,
+            }
+        }
+        .new_child()
+        .await
+        {
+            Ok(res) => Ok(res),
+            Err(err) => Err(err),
+        }
+    }
+}
