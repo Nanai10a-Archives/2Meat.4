9fea6b21aff38c17dddf64e44dc708f6b4acde73
Nanai10a <Nanai10a@outlook.jp>
2021-04-21 06:59:44 +0900

    🚧 他commandsの実装.

--- src/discord/impl_commands.rs
+++ src/discord/impl_commands.rs
@@ -23,3 +23,70 @@ impl NewCommand<RefWrap<DiscordTransceivers>> for DiscordTransceiver {
         }
     }
 }
+
+#[serenity::async_trait]
+impl MutCommand for RefWrap<DiscordTransceiver> {
+    async fn mut_com(&self) -> anyhow::Result<()> {
+        todo!()
+    }
+}
+
+#[serenity::async_trait]
+impl DropCommand for RefWrap<DiscordTransceiver> {
+    async fn drop_com(&self) -> anyhow::Result<()> {
+        let mut locked = self.lock().await;
+
+        match *locked {
+            None => Err(anyhow::Error::msg("not found (already deleted).")),
+            Some(_) => {
+                // FIXME: 削除時の処理の追加
+                *locked = None;
+                Ok(())
+            }
+        }
+    }
+}
+
+#[serenity::async_trait]
+impl SubscCommand<RefWrap<DiscordTransceiver>> for RefWrap<DiscordTransceiver> {
+    async fn subsc_com(&self, target: &RefWrap<DiscordTransceiver>) -> anyhow::Result<()> {
+        let mut self_locked = self.lock().await;
+        let self_ref = match self_locked.as_mut() {
+            None => todo!(),
+            Some(item) => item,
+        };
+
+        let mut target_locked = target.lock().await;
+        let target_ref = match target_locked.as_mut() {
+            None => todo!(),
+            Some(item) => item,
+        };
+
+        self_ref
+            .subscribe_push(target_ref.get_id(), target_ref.get_subscriber())
+            .unwrap();
+
+        Ok(())
+    }
+}
+
+#[serenity::async_trait]
+impl ExitCommand<RefWrap<DiscordTransceiver>> for RefWrap<DiscordTransceiver> {
+    async fn exit_com(&self, target: &RefWrap<DiscordTransceiver>) -> anyhow::Result<()> {
+        let mut self_locked = self.lock().await;
+        let self_ref = match self_locked.as_mut() {
+            None => todo!(),
+            Some(item) => item,
+        };
+
+        let target_locked = target.lock().await;
+        let target_ref = match target_locked.as_ref() {
+            None => todo!(),
+            Some(item) => item,
+        };
+
+        self_ref.subscribe_remove(target_ref.get_id()).unwrap();
+
+        Ok(())
+    }
+}
