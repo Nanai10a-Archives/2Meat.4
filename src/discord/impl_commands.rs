af98056d4cccc3783694fcd674a4fea17b896041
Nanai10a <Nanai10a@outlook.jp>
2021-04-21 06:59:18 +0900

    🎨 awaitがある時はpoll()の値を確保しないと次の処理の前に吹き飛ぶので, 変数として確保してあげている(改善).

--- src/discord/impl_commands.rs
+++ src/discord/impl_commands.rs
@@ -10,15 +10,14 @@ use std::sync::Arc;
 #[serenity::async_trait]
 impl NewCommand<RefWrap<DiscordTransceivers>> for DiscordTransceiver {
     async fn new_com(parent: &RefWrap<DiscordTransceivers>) -> anyhow::Result<RefWrap<Self>> {
-        match {
-            match (*parent.lock().await).as_mut() {
-                None => todo!(),
-                Some(item) => item,
-            }
-        }
-        .new_child()
-        .await
-        {
+        let mut locked = parent.lock().await;
+
+        let trcvs = match locked.as_mut() {
+            None => todo!(),
+            Some(item) => item,
+        };
+
+        match trcvs.new_child().await {
             Ok(res) => Ok(res),
             Err(err) => Err(err),
         }
