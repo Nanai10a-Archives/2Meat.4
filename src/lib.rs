a1d6a0057dfe9acffef343c3fd5c58b63f1e15e8
Nanai10a <Nanai10a@outlook.jp>
2021-05-03 18:02:18 +0900

    🚧 dataを格納する構造体の定義(一部前実装より修正)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(new_uninit)]
 #![feature(get_mut_unchecked)]
 
+pub mod model;
 pub mod transceiver;
---
+++ src/model/data.rs
@@ -0,0 +1,82 @@
+use std::fmt::{Display, Formatter, Write};
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use smallvec::SmallVec;
+use std::net::IpAddr;
+use tokio::fs::File;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FormattedData {
+    pub content: String,
+    pub attachments: SmallVec<[Attachments; 4]>,
+    pub author: Author,
+    pub timestamp: DateTime<Utc>,
+}
+
+impl Display for FormattedData {
+    fn fmt(&self, _: &mut Formatter<'_>) -> std::fmt::Result {
+        String::new().write_fmt(format_args!(
+            "{}\
+            \
+            {}\
+            \
+            at {}",
+            self.author, self.content, self.timestamp
+        ))
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Attachments {
+    pub a_type: AttachmentType,
+    pub url: String,
+    pub data: Option<File>,
+}
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+pub enum AttachmentType {
+    Image,
+    Audio,
+    Movie,
+    Other,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Author {
+    pub name: String,
+    pub nickname: Option<String>,
+    pub id: String,
+    pub place: Place,
+}
+
+impl Display for Author {
+    fn fmt(&self, _: &mut Formatter<'_>) -> std::fmt::Result {
+        match &self.nickname {
+            None => String::new().write_fmt(format_args!(
+                "{}: {} on {:?}",
+                self.name, self.id, self.place
+            )),
+            Some(nick) => String::new().write_fmt(format_args!(
+                "{} ({}): {} on {:?}",
+                nick, self.name, self.id, self.place
+            )),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+pub enum Place {
+    Discord { channel_id: u64 },
+    Twitter,
+    Mastodon, // TODO: server domainを書けるかも？
+    // Direct { protocol: Protocol, ip: IpAddr }, // TODO: 要検討
+    Unknown,
+    // TODO: Frontendが増える度に更新
+}
+
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
+pub enum Protocol {
+    Http,
+    WebSocket,
+}
