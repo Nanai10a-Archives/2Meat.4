83644dd5eaaa85652cde13fc9a0f3748bef9ead4
Nanai10a <Nanai10a@outlook.jp>
2021-05-03 18:07:51 +0900

    🚧 操作(command)の引数の定義.

---
+++ src/model/arg.rs
@@ -0,0 +1,18 @@
+use smallvec::SmallVec;
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub enum CommandArgs {
+    New,
+    Drop {
+        id: Uuid,
+    },
+    Subsc {
+        broadcaster_id: Uuid,
+        subscriber_id: Uuid,
+    },
+    Exit {
+        broadcaster_id: Uuid,
+        subscriber_id: Uuid,
+    },
+}
